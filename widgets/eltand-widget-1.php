5a064eba029b04ff14891ab5f678df665b03f432
D1R0 <38256732+D1R0@users.noreply.github.com>
2024-06-19 20:18:45 +0300

    Update eltand-widget-1.php

--- widgets/eltand-widget-1.php
+++ widgets/eltand-widget-1.php
@@ -35,14 +35,14 @@ class Eltand_Widget_1 extends \Elementor\Widget_Base
         <div class="customizeWebsite_etd col-12_etd" id="headSectionCustomizeWebsite" style="min-width:200px;">
             <div class="header_etd w-100_etd d-flex_etd flex-wrap_etd py-2_etd my-3_etd align-items-center_etd">
                 <div class="selectTab_etd col_etd">
-                    <h4 class="textPrimary_etd my-auto_etd">Tipul Website-ului</h4>
+                    <h4 class="textPrimary_etd my-auto_etd">Website type</h4>
                 </div>
                 <div class="col_etd text-right_etd">
                     <select name="" class="selectType_etd">
-                        <option selected disabled>Alege</option>
-                        <option value="ecomm">eCommerce(magazin)</option>
-                        <option value="pres">Prezentare</option>
-                        <option value="other">Alt Tip</option>
+                        <option selected disabled>Choose</option>
+                        <option value="ecomm">eCommerce</option>
+                        <option value="pres">Presentation</option>
+                        <option value="other">Other Type</option>
                     </select>
                 </div>
             </div>
@@ -55,9 +55,9 @@ class Eltand_Widget_1 extends \Elementor\Widget_Base
                 </div>
 
                 <div class="supportCard_etd mt-4_etd">
-                    <h3>Dificultati? Ai nevoie de ajutor?</h3>
+                    <h3> Do you need help?</h3>
                     <hr>
-                    <p>Trimite mail la <a href="mailto:official@eltand.com">official@eltand.com</a> sau apeleaza la numerele
+                    <p>Send mail to <a href="mailto:official@eltand.com">official@eltand.com</a> or call the number
                         <br><a href="tel:0750446970">+40 750 446 970</a> - Official Eltand
                     </p>
                 </div>
